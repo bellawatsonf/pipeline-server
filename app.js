6f198f2d1b84c1b979a29c09ef51284be61412c9
bellawatsonf <bellawatsonf@gmail.com>
2023-03-14 11:03:24 +0700

    fix env

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// require("dotenv").config();
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 // const axios = require("axios");
 const app = express();
