3a772ba2271d8de60cce2d5cc3eed88ee531ae2b
bellawatsonf <bellawatsonf@gmail.com>
2023-03-15 20:44:45 +0700

    add seed

---
+++ seeders/20230315131007-insert data admin into pegawai.js
@@ -0,0 +1,36 @@
+"use strict";
+const { encrypt } = require("../helper/bcrypt");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Pegawais",
+      [
+        {
+          nip: "12345",
+          password: encrypt("admin"),
+          nama_pegawai: "Admin",
+          posisi: "Admin",
+          lokasi: "Admin",
+          group_id: 1,
+          level: "Admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Pegawais", null, {});
+
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
