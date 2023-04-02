22a34239fd0d68f410836b1ce648fa878b821780
bellawatsonf <bellawatsonf@gmail.com>
2023-04-02 20:41:08 +0700

    edit pipeline

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV != "production") require("dotenv").config();
 const express = require("express");
 // const axios = require("axios");
 const app = express();
-// const port = 3000;
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const router = require("./routes");
